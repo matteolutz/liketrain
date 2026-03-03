03831ecac534e071e0ee38d7077a038611bf332a
Matteo Lutz <matteo.lutz06@outlook.de>
2026-03-03 18:59:23 +0100

    feat: began implementing rs485 support

---
+++ liketrain-arduino/lib/RS485/RS485.cpp
@@ -0,0 +1,56 @@
+#include "RS485.h"
+
+RS485::RS485(Stream *stream, uint8_t re_de_pin)
+    : stream(stream), re_de_pin(re_de_pin)
+{
+}
+
+RS485::RS485(HardwareSerial *hw_serial, uint8_t re_de_pin)
+    : stream(hw_serial), hw_serial(hw_serial), stream_impl(RS485StreamImplementation::HardwareSerial), re_de_pin(re_de_pin)
+{
+}
+
+void RS485::init(unsigned long baud) const
+{
+    switch (stream_impl)
+    {
+    case RS485StreamImplementation::HardwareSerial:
+        hw_serial->begin(baud);
+        break;
+    case RS485StreamImplementation::Unknown:
+        break;
+    }
+}
+
+void RS485::flush()
+{
+    stream->flush();
+
+    // wait for the last byte to be sent before switching back to receive mode.
+    // this is copied from https://github.com/RobTillaart/RS485
+    delayMicroseconds(RS485_MICROS_PER_BYTE);
+}
+
+size_t RS485::write(uint8_t byte)
+{
+    set_transmit_mode();
+
+    size_t bytes_written = stream->write(byte);
+    stream->flush();
+
+    set_receive_mode();
+
+    return bytes_written;
+}
+
+size_t RS485::write(const uint8_t *buffer, size_t size)
+{
+    set_transmit_mode();
+
+    size_t bytes_written = stream->write(buffer, size);
+    stream->flush();
+
+    set_receive_mode();
+
+    return bytes_written;
+}
---
+++ liketrain-arduino/lib/RS485/RS485.h
@@ -0,0 +1,55 @@
+#ifndef RS485_H
+#define RS485_H
+
+#include <Arduino.h>
+
+#define RS485_MICROS_PER_BYTE 1100
+
+enum class RS485Mode
+{
+    Receive,
+    Transmit
+};
+
+enum class RS485StreamImplementation
+{
+    Unknown,
+    HardwareSerial,
+};
+
+class RS485 : public Stream
+{
+public:
+    RS485(Stream *stream, uint8_t re_de_pin);
+    RS485(HardwareSerial *hw_serial, uint8_t re_de_pin);
+
+    // ~~~~~~~~~~~ Stream interface ~~~~~~~~~~ //
+    inline int available() override { return stream->available(); }
+    inline int read() override { return stream->read(); }
+    inline int peek() override { return stream->peek(); }
+
+    // ~~~~~~~~~~~ Print interface ~~~~~~~~~~~ //
+    void flush() override;
+
+    size_t write(uint8_t byte) override;
+    size_t write(const uint8_t *buffer, size_t size) override;
+
+    // ~~~~~~~~ RS485 specific methods ~~~~~~~ //
+    void init(unsigned long baud = 115200) const;
+
+    inline void set_mode(RS485Mode mode) { digitalWrite(re_de_pin, mode == RS485Mode::Transmit ? HIGH : LOW); };
+    inline RS485Mode get_mode() const { return digitalRead(re_de_pin) == LOW ? RS485Mode::Receive : RS485Mode::Transmit; }
+
+    inline void set_receive_mode() { set_mode(RS485Mode::Receive); }
+    inline void set_transmit_mode() { set_mode(RS485Mode::Transmit); }
+
+private:
+    Stream *stream;
+
+    HardwareSerial *hw_serial = nullptr;
+    RS485StreamImplementation stream_impl = RS485StreamImplementation::Unknown;
+
+    uint8_t re_de_pin;
+}
+
+#endif // RS485_H
